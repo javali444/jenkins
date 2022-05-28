347dd29cb930c0061cb1e7e5d9ee1f5e5f885e7b
Javali Valluru <javalivallururjsj44@gmail.com>
2022-05-29 00:39:49 +0530

    adding groovy code for creating folders

--- seed-jobs/a_folders.groovy
+++ seed-jobs/a_folders.groovy
@@ -2,7 +2,7 @@ folder('CI-pipeline') {
     displayName('CI-pipeline')
     description('CI-pipeline')
     authorization {
-        permission('admin:authenticated')
+        permission('hudson.model.Item.Create:authenticated')
     }
 }
 
