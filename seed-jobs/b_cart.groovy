aaf9859dd4cedbd44aff8aa76113ce191172ab68
Javali Valluru <javalivallururjsj44@gmail.com>
2022-05-29 00:46:59 +0530

    adding groovy code for creating folders

--- seed-jobs/b_cart.groovy
+++ seed-jobs/b_cart.groovy
@@ -1,8 +1,11 @@
 multibranchPipelineJob('CI-pipeline/cart') {
+    authorization {
+        permission('hudson.model.Item.Create:authenticated')
+    }
     branchSources {
         github {
             id('23232323') // IMPORTANT: use a constant and unique identifier
-            scanCredentialsId('GitHubToken')
+            scanCredentialsId('GitHub')
             repository('javali444/cart')
         }
     }
