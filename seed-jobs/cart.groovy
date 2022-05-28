b708df3aac68e1c9f6408cdac89a9191eac34d72
Javali Valluru <javalivallururjsj44@gmail.com>
2022-05-28 22:34:50 +0530

    adding groovy code for creating multi job pipeline for cart

--- seed-jobs/cart.groovy
+++ seed-jobs/cart.groovy
@@ -1,8 +1,8 @@
 multibranchPipelineJob('cart') {
     branchSources {
         git {
-            remote('https://github.com/javali444/cart.git')
-
+            id('1234') // IMPORTANT: use a constant and unique identifier
+            remote('https://github.com/javali444/jenkins.git')
         }
     }
     orphanedItemStrategy {
